ef662222be4924f97d87c66d524a7de09fc0283a
Rishi Khare <rishi.khare@berkeley.edu>
2022-05-19 23:03:05 -0500

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require("postman-request");
+
+const forecast = (lat, long, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=963a5be541f0087c83daee3df4d8efb9&query=" +
+    lat +
+    "," +
+    long +
+    "&units=f";
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect...", undefined);
+    } else if (body.error) {
+      callback("Unable to find forecast", undefined);
+      console.log(body.error);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
