fca3cf3582e18d125075e1bd502c82724b33fd76
Rishi Khare <rishi.khare@berkeley.edu>
2022-05-19 23:38:29 -0500

    Fixed typos in forecast summary

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,7 @@ const forecast = (lat, long, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`
       );
     }
   });
